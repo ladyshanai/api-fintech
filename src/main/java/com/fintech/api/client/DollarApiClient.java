3226baa0e24a813ed9d2f77c5bc41ec86dcdcc19
Paula <paulalezcano21@gmail.com>
2026-06-24 18:53:35 -0300

    feat(feign): update annotation url

--- src/main/java/com/fintech/api/client/DollarApiClient.java
+++ src/main/java/com/fintech/api/client/DollarApiClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@FeignClient(name = "dolar-api-client", url = "${dolar-api-client.api-dolar-url}")
+@FeignClient(name = "dolar-api-client")
 public interface DollarApiClient {
     @GetMapping("/v1/dolares/oficial")
     DollarModel getCotizacion();
